198011bebfce076a85845d83b8296a2591b53ac6
Tom Manley <tom.manley@gmail.com>
2015-04-18 13:35:50 -0500

    Initial commit for arduino powered dog cage opener

---
+++ dogcage_arduino/dogcage_arduino.ino
@@ -0,0 +1,29 @@
+#include <Servo.h> 
+ 
+Servo myservo;
+
+int pos = 0;
+const int servo_pin = 17;
+const int ledPin = 13;
+const int button_pin = 6;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(button_pin, INPUT);
+  myservo.attach(servo_pin);
+}
+
+void loop() {
+  if( digitalRead(button_pin) == HIGH) { 
+    digitalWrite(ledPin, HIGH);   // set the LED on
+    delay(500);                  // wait for a second
+    digitalWrite(ledPin, LOW);    // set the LED off
+    delay(500);   
+  
+    for(pos=0; pos<=180; pos+=1) {
+      myservo.write(pos);
+      delay(15);
+    }
+  }
+  delay(500);
+}

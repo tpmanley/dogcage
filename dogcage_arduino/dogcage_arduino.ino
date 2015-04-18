1d4c98bff7ac6bc07a04da5c42a2c4d9f4094076
Tom Manley <tom.manley@gmail.com>
2015-04-18 18:45:35 -0500

    Sucessfully opened cage door

--- dogcage_arduino/dogcage_arduino.ino
+++ dogcage_arduino/dogcage_arduino.ino
@@ -2,7 +2,12 @@
  
 Servo myservo;
 
+const int BUTTON_PRESSED = 1;
+const int BUTTON_NOT_PRESSED = 0;
+
 int pos = 0;
+int prev_button_status = BUTTON_NOT_PRESSED;
+
 const int servo_pin = 17;
 const int ledPin = 13;
 const int button_pin = 6;
@@ -10,19 +15,41 @@ const int button_pin = 6;
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(button_pin, INPUT);
+  digitalWrite(ledPin, LOW);
   myservo.attach(servo_pin);
+  myservo.write(45);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);   // set the LED on
-  delay(500);                  // wait for a second
-  digitalWrite(ledPin, LOW);    // set the LED off
-  delay(500);
+  delay(1000);
   
-  if( digitalRead(button_pin) == LOW) {
-    for(pos=0; pos<=180; pos+=1) {
-      myservo.write(pos);
-      delay(15);
+  if(prev_button_status == BUTTON_NOT_PRESSED)
+  {
+    if(digitalRead(button_pin) == LOW)
+    {
+      digitalWrite(ledPin, HIGH);
+      prev_button_status = BUTTON_PRESSED;
+      
+      for(pos=45; pos<=130; pos+=1)
+      {
+        myservo.write(pos);
+        delay(100);
+      }
+      myservo.write(45);
+    }
+  }
+  else
+  {
+    if(digitalRead(button_pin) == HIGH)
+    {
+      prev_button_status = BUTTON_NOT_PRESSED;
+      digitalWrite(ledPin, LOW);
+    }
+    else
+    {
+      digitalWrite(ledPin, LOW);
+      delay(200);
+      digitalWrite(ledPin, HIGH);
     }
   }
 }
